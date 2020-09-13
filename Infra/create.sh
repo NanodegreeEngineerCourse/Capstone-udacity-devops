0d161c4980d051742771074e247e576233854fee
NDE_luis.perez <luis.perez@nttdisruption.com>
2020-09-13 15:47:22 +0200

    New Infra, fixes on deployment filemand adjusting pipelines

--- Infra/create.sh
+++ Infra/create.sh
@@ -1,5 +1,6 @@
 aws cloudformation create-stack \
 --stack-name $1 \
 --template-body file://$2 \
+--parameters file://parameters.json \
 --region=eu-west-1 \
 --capabilities CAPABILITY_NAMED_IAM
--- Infra/update.sh
+++ Infra/update.sh
@@ -1,5 +1,6 @@
 aws cloudformation update-stack \
 --stack-name $1 \
 --template-body file://$2 \
+--parameters file://parameters.json \
 --region=eu-west-1 \
 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" 
